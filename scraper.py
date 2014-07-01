f533c3dc195eb42226ce132c563e70dc9f448727
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-06-30 17:41:45 -0700

    Created test file for scrapper.py

--- scraper.py
+++ scraper.py
@@ -23,42 +23,41 @@ def fetch_search_results(
 
 
 def parse_source(html, encoding='utf-8'):
-	parsed = BeautifulSoup(html, from_encoding=encoding)
-	return parsed
+    parsed = BeautifulSoup(html, from_encoding=encoding)
+    return parsed
 
 
 def extract_listings(parsed):
-	location_attrs = {'data-latitude': True, 'data-longitude' : True}
-	listings = parsed.find_all('p', class_='row') 
-	extracted = []
-	for listing in listings:
-		location = {key: listing.attrs.get(key, '') for key in location_attrs}
-		link = listing.find('span', class_='pl').find('a')
-		price_span = listing.find('span', class_='price')
-		this_listing = {'location':location,				'link': link.attrs['href'], 
-										'description': link.string.strip(),
-										'price': price_span.string.strip(),
-										'size': price_span.next_sibling.strip(' \n-/')}
-		extracted.append(this_listing)
-	return extracted
+    location_attrs = {'data-latitude': True, 'data-longitude' : True}
+    listings = parsed.find_all('p', class_='row')
+    extracted = []
+    for listing in listings:
+        location = {key: listing.attrs.get(key, '') for key in location_attrs}
+        link = listing.find('span', class_='pl').find('a')
+        price_span = listing.find('span', class_='price')
+        this_listing = {'location':location, 'link': link.attrs['href'],
+                                        'description': link.string.strip(),
+                                        'price': price_span.string.strip(),
+                                        'size': price_span.next_sibling.strip(' \n-/')}
+        extracted.append(this_listing)
+    return extracted
 
 
 def read_search_results():
-	with open('apartments.html', 'r') as the_file:
-		file_ = the_file.read()
-	
+    with open('apartments.html', 'r') as the_file:
+        file_ = the_file.read()
 
-	return file_, 'utf-8' #find how to extract encoding from file read.
+    return file_, 'utf-8' #find how to extract encoding from file read.
 
 if __name__ == '__main__':
-	import pprint
-	if len(sys.argv) > 1 and sys.argv[1] == 'test':
-		html, encoding = read_search_results()
-	else:
-		html, encoding = fetch_search_results(minAsk=500, maxAsk=1000, bedrooms=2)
+    import pprint
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        html, encoding = read_search_results()
+    else:
+        html, encoding = fetch_search_results(minAsk=500, maxAsk=1000, bedrooms=2)
 
-	doc = parse_source(html, encoding)
-	listings = extract_listings(doc)
-	print len(listings)
-	pprint.pprint(listings[0])
+    doc = parse_source(html, encoding)
+    listings = extract_listings(doc)
+    print len(listings)
+    pprint.pprint(listings[0])
 
