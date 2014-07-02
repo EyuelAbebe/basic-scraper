8f0dc082aa1dff73d2e952250b81a77638166c79
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-07-02 07:01:25 -0700

    Used json api for apt scraping. Modfied code accordingly.

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+import json
 
 
 def fetch_search_results(
@@ -22,6 +23,29 @@ def fetch_search_results(
     return resp.content, resp.encoding
 
 
+
+
+def fetch_json_results(
+        query=None, minAsk=None, maxAsk=None, bedrooms=None
+):
+    search_params = {
+        key: val for key, val in locals().items() if val is not None
+    }
+    if not search_params:
+        raise ValueError("No valid keywords")
+
+    base = 'http://maps.googleapis.com/maps/api/geocode/json'
+    resp = requests.get(base, params=search_params, timeout=3)
+    resp.raise_for_status()
+
+    with open('apartments.html', 'w') as the_file:
+        the_file.write(resp.content)
+
+    return resp.content, resp.encoding
+
+
+
+
 def parse_source(html, encoding='utf-8'):
     parsed = BeautifulSoup(html, from_encoding=encoding)
     return parsed
@@ -29,8 +53,7 @@ def parse_source(html, encoding='utf-8'):
 
 def extract_listings(parsed):
     location_attrs = {'data-latitude': True, 'data-longitude' : True}
-    listings = parsed.find_all('p', class_='row')
-    extracted = []
+    listings = parsed.find_all('p', class_='row', attrs=location_attrs)
     for listing in listings:
         location = {key: listing.attrs.get(key, '') for key in location_attrs}
         link = listing.find('span', class_='pl').find('a')
@@ -39,8 +62,7 @@ def extract_listings(parsed):
                                         'description': link.string.strip(),
                                         'price': price_span.string.strip(),
                                         'size': price_span.next_sibling.strip(' \n-/')}
-        extracted.append(this_listing)
-    return extracted
+        yield this_listing
 
 
 def read_search_results():
@@ -48,6 +70,28 @@ def read_search_results():
         file_ = the_file.read()
 
     return file_, 'utf-8' #find how to extract encoding from file read.
+
+
+def add_address(listing):
+    api_url = 'http://maps.googleapis.com/maps/api/geocode/json'
+    loc = listing['location']
+    latlng_tmpl = "{data-latitude},{data-longitude}"
+    parameters = {
+        'sensor': 'false',
+        'latlng': latlng_tmpl.format(**loc),
+    }
+    resp = requests.get(api_url, params=parameters)
+    resp.raise_for_status() # <- this is a no-op if all is well
+    data = json.loads(resp.text)
+    if data['status'] == 'OK':
+        best = data['results'][0]
+        listing['address'] = best['formatted_address']
+    else:
+        listing['address'] = 'unavailable'
+    return listing
+
+
+
 
 if __name__ == '__main__':
     import pprint
@@ -57,7 +101,7 @@ if __name__ == '__main__':
         html, encoding = fetch_search_results(minAsk=500, maxAsk=1000, bedrooms=2)
 
     doc = parse_source(html, encoding)
-    listings = extract_listings(doc)
-    print len(listings)
-    pprint.pprint(listings[0])
+    for listing in extract_listings(doc):
+        listing = add_address(listing)
+        pprint.pprint(listing)
 
