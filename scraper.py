5c5ecdff3c9ef4839fc6ad6c7d4838b3bda784a5
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-07-12 21:09:12 -0700

    Made cahnges to scraper.py to include googles json api and use generators for extracting function.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
-import json
+import sys, json
 
 
 def fetch_search_results(
@@ -23,29 +22,6 @@ def fetch_search_results(
     return resp.content, resp.encoding
 
 
-
-
-def fetch_json_results(
-        query=None, minAsk=None, maxAsk=None, bedrooms=None
-):
-    search_params = {
-        key: val for key, val in locals().items() if val is not None
-    }
-    if not search_params:
-        raise ValueError("No valid keywords")
-
-    base = 'http://maps.googleapis.com/maps/api/geocode/json'
-    resp = requests.get(base, params=search_params, timeout=3)
-    resp.raise_for_status()
-
-    with open('apartments.html', 'w') as the_file:
-        the_file.write(resp.content)
-
-    return resp.content, resp.encoding
-
-
-
-
 def parse_source(html, encoding='utf-8'):
     parsed = BeautifulSoup(html, from_encoding=encoding)
     return parsed
@@ -58,10 +34,11 @@ def extract_listings(parsed):
         location = {key: listing.attrs.get(key, '') for key in location_attrs}
         link = listing.find('span', class_='pl').find('a')
         price_span = listing.find('span', class_='price')
-        this_listing = {'location':location, 'link': link.attrs['href'],
-                                        'description': link.string.strip(),
-                                        'price': price_span.string.strip(),
-                                        'size': price_span.next_sibling.strip(' \n-/')}
+        this_listing = {
+            'location': location, 'link': link.attrs['href'],
+            'description': link.string.strip(),
+            'price': price_span.string.strip(),
+            'size': price_span.next_sibling.strip(' \n-/')}
         yield this_listing
 
 
@@ -69,7 +46,7 @@ def read_search_results():
     with open('apartments.html', 'r') as the_file:
         file_ = the_file.read()
 
-    return file_, 'utf-8' #find how to extract encoding from file read.
+    return file_, 'utf-8'
 
 
 def add_address(listing):
@@ -91,17 +68,15 @@ def add_address(listing):
     return listing
 
 
-
-
 if __name__ == '__main__':
     import pprint
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         html, encoding = read_search_results()
     else:
-        html, encoding = fetch_search_results(minAsk=500, maxAsk=1000, bedrooms=2)
-
+        html, encoding = fetch_search_results(
+            minAsk=500, maxAsk=1000, bedrooms=2
+        )
     doc = parse_source(html, encoding)
     for listing in extract_listings(doc):
         listing = add_address(listing)
         pprint.pprint(listing)
-
